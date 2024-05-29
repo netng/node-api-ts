6a2cb469072f102cbd1e6010ff2815bd786ca7f2
nandang <net.nandang@gmail.com>
2024-05-29 16:57:24 +0700

    add main file of the entire project

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+// Create express server
+const app = express(); // This is new express instance
+const port = 3000; // express http port
+
+// Express config
+app.use(cors()) // enable cors
+app.use(helmet()) // enable helmet
+app.use(morgan('dev')) // enable morgan
+
+// Start express server
+app.listen(port, () => {
+    // Callback function when server is successfully started
+    console.log(`Server started at http://localhost:${port}`)
+})
+
+// Export express app
+export default app
