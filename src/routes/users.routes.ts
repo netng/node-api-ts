020bbfd8aee2b54d60a36fd7b694217d560f46e7
nandang <net.nandang@gmail.com>
2024-05-30 14:45:13 +0700

    add user validation middleware for validate any data before gets to the controller when creting and updating the data

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,13 +6,25 @@ import {
     getUsersController,
     updateUserController 
 } from "../controllers/users.controllers";
+import { validateUser } from "../utils/validations/users.validations";
 
 const router = Router()
 
 router.get('/', getUsersController)
 router.get('/:id', getUserByIdController)
-router.post('/', createUserController)
-router.put('/:id', updateUserController)
+
+router.post(
+    '/',
+    validateUser,
+    createUserController
+)
+
+router.put(
+    '/:id',
+    validateUser,
+    updateUserController
+)
+
 router.delete('/:id', deleteUserController)
 
 export default router;
