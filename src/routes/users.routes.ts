25a1222c6130f6f353760c88b8c10374dee4356b
nandang <net.nandang@gmail.com>
2024-05-30 10:32:44 +0700

    update user.routes to use our user.controller functionality

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,13 +1,18 @@
 import { Router, Request, Response } from "express"
+import { 
+    createUser,
+    deleteUser,
+    getUserById,
+    getUsers,
+    updateUser 
+} from "../models/users.models";
 
 const router = Router()
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('Users route!')
-})
-
-router.get('/:id', (req: Request, res: Response) => {
-    res.send(`User ${req.params.id} route!`)
-})
+router.get('/', getUsers)
+router.get('/:id', getUserById)
+router.post('/', createUser)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 export default router;
