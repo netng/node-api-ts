3caa846d20d3429cf0956d21a1b6f80554a43dac
nandang <net.nandang@gmail.com>
2024-05-30 10:58:00 +0700

    update users model crud business logic

--- src/models/users.models.ts
+++ src/models/users.models.ts
@@ -33,3 +33,17 @@ export const getUsers = (): Array<User> => {
 export const getUserById = (id: number): User | undefined => {
     return users.find((user) => user.id === id)
 }
+
+export const createUser = (user: User): void => {
+    users.push(user)
+}
+
+export const updateUser = (user: User): void => {
+    const index = users.findIndex((u) => u.id === user.id)
+    users[index] = user;
+}
+
+export const deleteUser = (id: number): void => {
+    const index = users.findIndex((u) => u.id === id)
+    users.splice(index, 1)
+}
